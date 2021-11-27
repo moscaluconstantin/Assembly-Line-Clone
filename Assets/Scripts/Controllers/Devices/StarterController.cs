1230700b195db3e123b08bb2f0d4d4c656f25c87
constantin-moscalu <moscaluconstantin1@gmail.com>
2021-11-27 19:32:08 +0300

    Add StarterController

---
+++ Assets/Scripts/Controllers/Devices/StarterController.cs
@@ -0,0 +1,15 @@
+﻿using Controllers.Base;
+
+namespace Controllers.Devices
+{
+    public class StarterController : SelectableController
+    {
+        protected override void OnSelected()
+        {
+        }
+
+        protected override void OnDeselected()
+        {
+        }
+    }
+}
